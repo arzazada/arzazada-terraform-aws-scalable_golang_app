d8e10437d7dd3d01a7d5974151209eeec78a6635
Abbas Rzazada <109742180+arzazada@users.noreply.github.com>
2024-03-07 15:06:20 +0400

    init

--- modules/scalable_golang_app/notification.tf
+++ modules/scalable_golang_app/notification.tf
@@ -70,73 +70,6 @@ resource "aws_iam_role" "delivery_feedback_role" {
   }
 }
 
-#resource "aws_sns_topic" "notif" {
-#  name                             = "${var.project_name}-cp-notification"
-#  lambda_failure_feedback_role_arn = aws_iam_role.delivery_feedback_role.arn
-#  lambda_success_feedback_role_arn = aws_iam_role.delivery_feedback_role.arn
-#}
-#
-#resource "aws_sns_topic_policy" "topic_policy22" {
-#  arn    = aws_sns_topic.notif.arn
-#  policy = jsonencode({
-#    Version = "2012-10-17",
-#    Statement = [
-#      {
-#        Action = ["SNS:Publish"],
-#        Effect = "Allow",
-#        Principals = {
-#          Service = "codestar-notifications.amazonaws.com"
-#        },
-#        Resources = [aws_sns_topic.notif.arn]
-#      }
-#    ]
-#  })
-#}
-#
-#resource "aws_sns_topic_subscription" "topic_subscription" {
-#
-#  topic_arn = aws_sns_topic.notif.arn
-#  protocol  = "lambda"
-#  endpoint  = aws_lambda_function.status_lambda.arn
-#}
-#
-#data "aws_iam_policy_document" "feedback_assume_role_policy" {
-#  statement {
-#    actions = ["sts:AssumeRole"]
-#    principals {
-#      type        = "Service"
-#      identifiers = ["sns.amazonaws.com"]
-#    }
-#  }
-#}
-#
-#resource "aws_iam_role" "delivery_feedback_role" {
-#  name               = "SNSFeedbackRole"
-#  assume_role_policy = data.aws_iam_policy_document.feedback_assume_role_policy.json
-#
-#  inline_policy {
-#    name = "SNSFeedbackPolicy"
-#
-#    policy = jsonencode({
-#      "Version" : "2012-10-17",
-#      "Statement" : [
-#        {
-#          "Effect" : "Allow",
-#          "Action" : [
-#            "logs:CreateLogGroup",
-#            "logs:CreateLogStream",
-#            "logs:PutLogEvents",
-#            "logs:PutMetricFilter",
-#            "logs:PutRetentionPolicy"
-#          ],
-#          "Resource" : [
-#            "*"
-#          ]
-#        }
-#      ]
-#    })
-#  }
-#}
 ##### CodeStar
 
 # https://docs.aws.amazon.com/dtconsole/latest/userguide/concepts.html#concepts-api
@@ -226,64 +159,6 @@ resource "aws_iam_role_policy" "lambda_role_policy" {
     ]
   })
 }
-
-
-
-
-
-#
-#resource "aws_iam_role" "lambda_role" {
-#  name               = "codepipeline-notification-lambda-role"
-#  assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
-#}
-#
-#data "aws_iam_policy_document" "assume_role_lambda" {
-#  statement {
-#    actions = ["sts:AssumeRole"]
-#    principals {
-#      type        = "Service"
-#      identifiers = ["lambda.amazonaws.com"]
-#    }
-#  }
-#}
-#
-#resource "aws_iam_policy" "lambda_policy" {
-#  name   = "codepipeline-notification-lambda-policy"
-#  policy = data.aws_iam_policy_document.lambda_policy.json
-#}
-#
-#data "aws_iam_policy_document" "lambda_policy" {
-#  statement {
-#    actions = [
-#      "logs:CreateLogGroup",
-#      "logs:CreateLogStream",
-#      "logs:PutLogEvents"
-#    ]
-#    effect    = "Allow"
-#    resources = ["*"]
-#  }
-#
-#  statement {
-#    actions   = ["ssm:GetParameter"]
-#    effect    = "Allow"
-#    resources = ["arn:aws:ssm:${local.region}:${local.account_id}:parameter/${var.project_name}/GITHUB_TOKEN"]
-#  }
-#
-#  statement {
-#    actions = [
-#      "codepipeline:GetPipelineExecution",
-#      "codepipeline:ListActionExecutions"
-#    ]
-#    effect    = "Allow"
-#    resources = aws_codepipeline.scalable_golang_app_pipeline.arn
-#  }
-#
-#}
-#
-#resource "aws_iam_role_policy_attachment" "lambda_role_attached_policy" {
-#  role       = aws_iam_role.lambda_role.name
-#  policy_arn = aws_iam_policy.lambda_policy.arn
-#}
 
 
 
